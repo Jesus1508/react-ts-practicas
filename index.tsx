c245ed8ba0bef63d1e5c12142d56f589c1f259a4
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 17:35:20 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -6,14 +6,20 @@ const Saludo = () => {
   const [name, setName] = useState('');
   return (
     <div>
-      <input type="text" />
+      {/* SyntheticEvent */}
+      <input type="text" onChange={(ev) => setName(ev.target.value)} />
       <p>Hola {name} </p>
+      {/* <a href="#" onClick={(ev) => ev.preventDefault()}>Ir</a> */}
     </div>
   );
 };
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Saludo />
+    </div>
+  );
 };
 
 render(<App />, document.getElementById('react-app'));
