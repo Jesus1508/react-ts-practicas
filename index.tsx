d967cf9d4fcc7d50f2459cb8b76a1644d16a7ae4
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 14:59:41 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 
-componentDidMount();
-componentDidUpdate();
-componentWillUnmount();
-
 const Button = () => {
-  // Aqui no debe haber efectos secundarios
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('Ejecutado');
+  });
   return <button>Enviar</button>;
 };
 
 const App = () => <Button />;
 
 render(<App />, document.getElementById('react-app'));
-
-function componentDidUpdate() {
-  throw new Error('Function not implemented.');
-}
-
-function componentDidMount() {
-  throw new Error('Function not implemented.');
-}
-
-function componentWillUnmount() {
-  throw new Error('Function not implemented.');
-}
