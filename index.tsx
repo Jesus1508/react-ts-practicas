bc5e6096e48fed8c6226ff65d60d6bab78cccf6b
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 07:31:31 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,45 +1,24 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-const nombres = ['Merce', 'Jesus', 'Dafhne', 'Lia'];
-
-function getNombres() {
-  const elementosLista = [];
-  for (var i = 0; i < nombres.length; i++) {
-    elementosLista.push(<li>{nombres[i]}</li>);
-  }
-}
-
-const Nombres = () => {
-  return <ul> {getNombres()} </ul>;
+const Saludo = () => {
+  const nombre = 'Jesus';
+  const idioma = 'es';
+  return [
+    <div>
+      {
+        idioma === 'en' ? <p>Hello</p> : <p>Hola</p>
+      }
+    </div>
+  ];
 };
 
 const App = () => {
   return (
-    <div>
-      <Nombres />
-    </div>
+    <h1>
+      <Saludo />
+    </h1>
   );
 };
-
-// const Saludo = () => {
-//   const nombre = 'Jesus';
-//   const idioma = 'en';
-//   return [
-//     <div>
-//       {
-//         idioma === 'en' ? <p>Hello</p> : <p>Hola</p>
-//       }
-//     </div>
-//     ];
-// };
-
-// const App = () => {
-//   return (
-//     <h1>
-//       <Saludo />
-//     </h1>
-//   );
-// };
 
 render(<App />, document.getElementById('react-app'));
