147ee12be226424eba29a487f73ef9c84244487e
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 07:57:44 -0500

    2 files added, 1 deleted

---
+++ index_02Ciclos.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const nombres = ['Merce', 'Dafhne', 'Lia', 'Jesus'];
+
+/*
+function getNombres() {
+  const elementosLista = [];
+  for (var i = 0; i < nombres.length; i++) {
+    elementosLista.push(<li>{nombres[i]}</li>);
+  }
+  return elementosLista;
+}
+*/
+const Nombres = () => {
+  return (
+    <ul>
+      {nombres.map((nombre) => (
+        <li>{nombre}</li>
+      ))}
+    </ul>
+  );
+  //return <ul>{getNombres()}</ul>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Nombres />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('react-app'));
