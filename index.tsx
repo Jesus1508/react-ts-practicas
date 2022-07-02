27e277beb7614e7b779942a95281d8fb324a1c45
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 17:29:24 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import { useState }  from 'react';
+import { useState } from 'react';
 import { render } from 'react-dom';
 
-const Saludo = ()=>{
+const Saludo = () => {
+  const [name, setName] = useState('');
+  return (
+    <div>
+      <input type="text" />
+      <p>Hola {name} </p>
+    </div>
+  );
+};
 
-}
+const App = () => {
+  return <div></div>;
+};
 
-const App = ()=>{
-  return <div></div>
-}
-
-render (<App/>, document.getElementById('react-app'))
+render(<App />, document.getElementById('react-app'));
