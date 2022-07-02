45b172efd0b951cb78747b4ab5b6b7eb5bbdc0a6
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 18:24:41 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -6,8 +6,28 @@ const Form = () => {
   let [title, setTitle] = useState('');
   let [body, setBody] = useState('');
 
+  const sendForm = (ev: { preventDefault: () => void }) => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'title',
+        body: 'body',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setTitle('');
+        setBody('');
+        console.log(json);
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={(ev) => sendForm(ev)}>
       <div>
         <label htmlFor="title">Título</label>
         <input
