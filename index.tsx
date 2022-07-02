e76c445988143d4aaa6d1c47c1eb9287d3f01967
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 00:57:03 -0500

    1 file modified

---
+++ index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const Saludo = ()=>{
+  const nombre = "Jesus Solis";
+  const idioma = "en";
+  return{
+    <div>
+      {
+        idioma === "en" ? <p>Hello</p> : <p>Hola</p>
+      }
+    </div>
+  }
+}
+
+const App = () =>{
+  return <h1><Saludo/></h1>;
+}
+
+render (<App/>, document.getElementById('react-app'))
