0e81241f27bf3b81b7a8c5e24a3cf57ede21602f
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 01:16:40 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+const nombres = ['Merce', 'Jesus', 'Dafhne', 'Lia'];
+
+function getNombres() {
+  const elementosLista = [];
+  for (var i = 0; i < nombres.length; i++) {
+    elementosLista.push(<li>{nombres[i]}</li>);
+  }
+}
+
+const Nombres = () => {
+  return <ul> {getNombres()} </ul>;
+};
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Nombres />
+    </div>
+  );
 };
 
 // const Saludo = () => {
