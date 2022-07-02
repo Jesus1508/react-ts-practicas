e18499e56ea0239093d08b27c1e7964ed321ab94
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 15:31:03 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -6,10 +6,30 @@ const Button = () => {
   const [count, setCount] = useState(0);
   useEffect(() => {
     console.log('Ejecutado');
-  });
-  return <button>Enviar</button>;
+    return () => {
+      console.log('Adios');
+    };
+  }, []);
+
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      Enviar
+    </button>
+  );
 };
 
-const App = () => <Button />;
+const App = () => {
+  const [showButton, setShowButton] = useState(true);
+  return (
+    <div>
+      <button onClick={() => setShowButton(false)}> Eliminar botón</button>
+      <div>{showButton && <Button />}</div>
+    </div>
+  );
+};
 
 render(<App />, document.getElementById('react-app'));
