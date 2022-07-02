6f54137e463e9531d1623b784ad9d543193d3757
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 17:58:53 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,8 +2,28 @@ import * as React from 'react';
 import { useState } from 'react';
 import { render } from 'react-dom';
 
+const Form = () => {
+  return (
+    <form>
+      <div>
+        <label for="title">Título</label>
+        <input type="text" id="title" />
+      </div>
+      <div>
+        <label for="body">Publicación</label>
+        <textarea id="body"></textarea>
+      </div>
+      <input type="submit" value="Enviar" />
+    </form>
+  );
+};
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 };
 
 render(<App />, document.getElementById('react-app'));
