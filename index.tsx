f2cd062ecf03ae2de2a98460a6e94b61ba7dfc3e
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 18:02:39 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -3,15 +3,25 @@ import { useState } from 'react';
 import { render } from 'react-dom';
 
 const Form = () => {
+  let [title, setTitle] = useState('');
+  let [body, setBody] = useState('');
+
   return (
     <form>
       <div>
         <label for="title">Título</label>
-        <input type="text" id="title" />
+        <input
+          type="text"
+          id="title"
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
       </div>
       <div>
         <label for="body">Publicación</label>
-        <textarea id="body"></textarea>
+        <textarea
+          id="body"
+          onChange={(ev) => setBody(ev.target.value)}
+        ></textarea>
       </div>
       <input type="submit" value="Enviar" />
     </form>
