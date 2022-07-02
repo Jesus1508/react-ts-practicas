10f45847ae874f97ba9810cdf0f343aa90d7f1a5
Jesus1508 <iscjesus1508@gmail.com>
2022-07-02 09:00:49 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,10 +2,20 @@ import * as React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
 
-class App extends Component {
-  render() {
-    return <h1>Hola mundo</h1>;
-  }
-}
+const nombres = ['Merce', 'Dafhne', 'Lia', 'Jesus'];
+
+const Saludar = () => {
+  const nombre = 'Jesus';
+
+  return <p>Hola {nombre}</p>;
+};
+
+const App = () => {
+  return (
+    <h1>
+      <Saludar />
+    </h1>
+  );
+};
 
 render(<App />, document.getElementById('react-app'));
